cb95d8cf189946e948abacac775c7940424bf849
pengfei <pengfeiye1994@gmail.com>
2017-10-16 16:57:22 -0700

    backend-data unfinished push 1

---
+++ seed.js
@@ -0,0 +1,22 @@
+var db = require('./modules');
+
+
+
+var test-destination =[{
+  location: "Taiwan",
+  date: "November-12-2017",
+  budget: 5000,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Author'
+  },
+  plans: ["dsd","dsfsd","fsfs"]
+},{
+  location: "jfklsd",
+  date: "djklsa-24-294"
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'Author'
+  },
+  plans: ["fsf","dsda","fsfs"]
+}]
