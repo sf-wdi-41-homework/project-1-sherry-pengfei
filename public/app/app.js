b726e4e8154dd36424adc2579607a9065bc6feaa
pengfei <pengfeiye1994@gmail.com>
2017-10-17 13:40:00 -0700

    Delete and Create function and Data set

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,12 @@
 var destination =[]
+var counter;
 
 
 $(document).ready(function(){
+
+  $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
 
 //print destination data base on #htmlTarget
    $.ajax({
@@ -14,29 +19,83 @@ $(document).ready(function(){
 
 
    function getDestination(destination){
-     return `<hr>
+     var renderedHTML = '';
+     console.log(destination);
+     renderedHTML += `<hr>
              <div class="row">
-             <div class="col-md-8 col-md-offset-2">
-             <p>Location: ${desitnation.location}</p>
+             <div class="col-md-6 col-md-offset-3 location">
+             <div class="col-md-10">
+             <p>Location: ${destination.location}</p>
              <p>Date: ${destination.date}</p>
-             <p>Budget: $${destination.budget}</p>
-             <p>Plan: ${destination.plans}</p>
-             </div>
-             </div>`
+             <p>Budget: $ ${destination.budget}</p>
+             <p>Plans:</p>
+             <ul>`
+     for(var i = 0; i<destination.plans.length; i++){
+       var todo = destination.plans[i];
+       renderedHTML += `<li>${todo}</li>`
+
+      }
+      renderedHTML +=
+      `</ul>
+      </div>
+      <div class="col-md-2">
+      <button type="button" class="deleteBtn btn btn-danger" data-id=${destination._id}>Delete</button>
+      </div>
+      </div>
+      </div>`
+     return renderedHTML;
    }
 
-   function getDestinationHTML(destinations){
-     return destinations.map(getDestination)
+   function getDestinationHTML(destination){
+     return destination.map(getDestination)
    }
+
+
+   $("#htmlTarget").on("click", ".deleteBtn", function(event){
+     $.ajax({
+       method: "DELETE",
+       url: '/destination/'+$(this).attr('data-id'),
+       success: function deleteLocation(json){
+         console.log(json);
+         for(var i= 0; i<destination.length; i++){
+           if(destination[i]._id === json._id){
+           destination.splice(i,1);
+           break;};
+         }
+         reload();
+       },
+       error: function(a,b,c){
+         console.log(a,b,c)
+       }
+     })
+   })
+
+   $("#createTrip").on("submit", function(event){
+     event.preventDefault();
+     alert("BANG")
+     console.log($(this).serialize());
+     $.ajax({
+       method: "POST",
+       url: '/destination',
+       data: $(this).serialize(),
+       success: function create(json){
+         destination.push(json);
+         $("#createTrip input").val("");
+         reload();
+       },
+       error: function(a,b,c){console.log(a,b,c)}
+     })
+   })
+
 
    function reload(){
      $('#htmlTarget').empty();
-     var printDestination = getDestinationHTML(destination);
-     $('#htmlTarget').append(printDestination)
+     $('#htmlTarget').append(getDestinationHTML(destination))
    }
 
   function handleSuccess(response){
     destination=response;
+    console.log(destination);
     reload();
   }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   db = require('./modules');
+  controller = require('./controller')
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -18,8 +19,37 @@ app.get('/', function(req, res){
 });
 
 app.get('/destination', function(req, res){
-  
+  db.Destination.find(function(err,located){
+    if(err){return console.log(err)};
+    res.json(located)
+  })
 })
+
+app.post('/destination', function(req, res){
+
+  req.body.plans = req.body.plans.split(',').map(function(item) { return item.trim(); } );
+
+  console.log(req.body);
+  var newDestination = new db.Destination({
+    location: req.body.location,
+    date: req.body.date,
+    budget: req.body.budget,
+    plans: req.body.plans,
+  });
+
+  newDestination.save(function(err, yay){
+    if(err){return console.log(err)};
+    res.json(yay)
+  })
+})
+
+app.delete('/destination/:id', function(req, res){
+  db.Destination.findOneAndRemove({_id:req.params.id}, function(err, destroy){
+    if(err){return console.log(err)};
+    res.json(destroy);
+  })
+})
+
 
 
 
