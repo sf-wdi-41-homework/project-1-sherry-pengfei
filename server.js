2c0cd613398f4cd5c176fcc55347846ca726b984
pengfei <pengfeiye1994@gmail.com>
2017-10-20 11:16:03 -0700

    Edit bug fix and google map api added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  db = require('./models'),
-  controller = require('./controller'),
-  session = require('express-session'),
-  bcrypt = require('bcryptjs'),
-  passport = require('passport');
+    bodyParser = require('body-parser'),
+    db = require('./models'),
+    controller = require('./controller'),
+    session = require('express-session'),
+    bcrypt = require('bcryptjs'),
+    passport = require('passport');
 
 
 
@@ -16,13 +16,15 @@ app.use(express.static('public'));
 
 // Express Session Middleware
 app.use(session({
- secret: 'secret',
- resave: true,
- saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
 
 // body parser config to accept our datatypes
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 require('./config/passport')(passport);
 
@@ -30,135 +32,144 @@ require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('*', function(req, res, next){
-
-  res.locals.user = req.user || null ;
-  next();
+app.get('*', function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
 });
 
-
-
-app.get('/', function(req, res){
-  // where is the views folder?
- res.sendFile('views/index.html', {root: __dirname});
+app.get('/', function(req, res) {
+    // where is the views folder?
+    res.sendFile('views/index.html', {
+        root: __dirname
+    });
 });
 
-app.get('/destination', function(req, res){
-  db.Destination.find(function(err,located){
-    if(err){return console.log(err)};
-    res.json(located)
-  })
+app.get('/destination', function(req, res) {
+    db.Destination.find(function(err, located) {
+        if (err) {
+            return console.log(err)
+        };
+        res.json(located)
+    })
 })
 
-app.post('/destination', function(req, res){
+app.post('/destination', function(req, res) {
 
-  req.body.plans = req.body.plans.split(',').map(function(item) { return item.trim(); } );
+    req.body.plans = req.body.plans.split(',').map(function(item) {
+        return item.trim();
+    });
 
-  console.log(req.body);
-  var newDestination = new db.Destination({
-    organizer: req.body.organizer,
-    location: req.body.location,
-    date: req.body.date,
-    budget: req.body.budget,
-    plans: req.body.plans,
-  });
+    console.log(req.body);
+    var newDestination = new db.Destination({
+        organizer: req.body.organizer,
+        location: req.body.location,
+        date: req.body.date,
+        budget: req.body.budget,
+        plans: req.body.plans,
+    });
 
-  newDestination.save(function(err, yay){
-    if(err){return console.log(err)};
-    res.json(yay)
-  })
+    newDestination.save(function(err, yay) {
+        if (err) {
+            return console.log(err)
+        };
+        res.json(yay)
+    })
 })
 
-app.delete('/destination/:id', function(req, res){
-  db.Destination.findOneAndRemove({_id:req.params.id}, function(err, destroy){
-    if(err){return console.log(err)};
-    res.json(destroy);
-  })
+app.delete('/destination/:id', function(req, res) {
+    db.Destination.findOneAndRemove({
+        _id: req.params.id
+    }, function(err, destroy) {
+        if (err) {
+            return console.log(err)
+        };
+        res.json(destroy);
+    })
 })
 
 //update Location
-app.put('/destination/:id', function(req, res){
-  db.Destination.findOne({_id:req.params.id}, function(err,success){
-    success.organizer=req.body.updateOrganizer;
-    success.location=req.body.updateLocation;
-    success.date = req.body.updateDate;
-    success.budget = req.body.updateBudget;
-    success.plans = req.body.updatePlans.split(',').map(function(item) { return item.trim(); } );
-    success.save(function(err, update){
-      if(err){return}
-      res.json(update)
+app.put('/destination/:id', function(req, res) {
+    db.Destination.findOne({
+        _id: req.params.id
+    }, function(err, success) {
+        success.organizer = req.body.updateOrganizer;
+        success.location = req.body.updateLocation;
+        success.date = req.body.updateDate;
+        success.budget = req.body.updateBudget;
+        success.plans = req.body.updatePlans.split(',').map(function(item) {
+            return item.trim();
+        });
+        success.save(function(err, update) {
+            if (err) {
+                return
+            }
+            res.json(update)
+        })
     })
-  })
 });
 
-
-
 //user registration
-app.get('/users/register', function(req, res){
-  res.sendFile('views/register.html', {root: __dirname});
+app.get('/users/register', function(req, res) {
+    res.sendFile('views/register.html', {
+        root: __dirname
+    });
 });
 
 //register proccess
-app.post('/users/register', function(req, res){
-  console.log('testing');
+app.post('/users/register', function(req, res) {
+    console.log('testing');
 
-  const name = req.body.name;
-  const email = req.body.email;
-  const username = req.body.username;
-  const password = req.body.password;
-  const password2 = req.body.password2;
+    const name = req.body.name;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const password2 = req.body.password2;
 
-  var newUser = new db.User({
-    name:name,
-    email:email,
-    username:username,
-    password:password
-  });
+    var newUser = new db.User({
+        name: name,
+        email: email,
+        username: username,
+        password: password
+    });
 
-  bcrypt.genSalt(10, function(err, salt){
-      bcrypt.hash(newUser.password, salt, function(err, hash){
-          if(err){
-              console.log(err);
-          } else {
-              newUser.password = hash;
-              newUser.save(function(err){
-                  if(err){
-                      console.log(err);
-                      return;
-                  } else {
-                      console.log("user has been created..");
-                      res.redirect('/users/login');
-                  }
-              });
-          }
-      });
-  });
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            if (err) {
+                console.log(err);
+            } else {
+                newUser.password = hash;
+                newUser.save(function(err) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    } else {
+                        console.log("user has been created..");
+                        res.redirect('/users/login');
+                    }
+                });
+            }
+        });
+    });
 });
 
-app.get('/users/login', function(req,res){
-  res.sendFile('views/login.html', {root: __dirname});
+app.get('/users/login', function(req, res) {
+    res.sendFile('views/login.html', {
+        root: __dirname
+    });
 });
 
 
 // login Process
-app.post('/users/login', function(req, res, next){
+app.post('/users/login', function(req, res, next) {
     passport.authenticate('local', {
-      successRedirect:'/',
-      failureRedirect:'/users/login',
-      failureFlash:true
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
 
     })(req, res, next);
 });
 
 
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server is up and running on http://localhost:3000/');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express server is up and running on http://localhost:3000/');
 });
